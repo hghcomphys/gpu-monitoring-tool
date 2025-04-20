3cda4c921b584d8d4bd7565321686caf9f86076d
Hossein Ghorbanfekr <hgh.comphys@gmail.com>
2025-04-20 11:04:36 +0200

    added setup files

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='gtop',
+    version='0.1',
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'gtop = gtop.cli:main',
+        ],
+    },
+    install_requires=[
+        'nvidia-ml-py>=11.450.129',
+        'plotext>=5.2.8',
+    ],
+)
+
